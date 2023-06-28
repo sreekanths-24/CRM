f700580c4a78b0be4f4d6bf4ec104a0865a1871a
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2023-06-28 12:03:55 +0530

    trying to host this project using vercel

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
