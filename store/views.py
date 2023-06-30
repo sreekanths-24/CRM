c6ef9042f383103b57972f41bd2068e7879e2595
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2023-06-30 13:29:09 +0530

    added a feature that deletes all the feedbacks of a product automatically when the product is deleted

--- store/views.py
+++ store/views.py
@@ -7,7 +7,8 @@ from .utils import cookieCart, cartData, guestOrder
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from features.models import FeedbackTable
-from django.db.models import Count
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 def store(request):
 	data = cartData(request)
@@ -114,7 +115,7 @@ def previous_order(request):
 
 def review(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    feedback_list = FeedbackTable.objects.filter(product=product.name)
+    feedback_list = FeedbackTable.objects.filter(product=product.name).order_by('-created_at')
     feedback_count = feedback_list.count()  # Count the number of feedbacks
     context = {
         'product': product,
@@ -122,15 +123,9 @@ def review(request, pk):
         'feedback_count': feedback_count,
     }
     return render(request, 'store/reviews.html', context)
-# def previous_order(request):
-# 	if 'q1' in request.GET:
-# 		q1 = request.GET['q1']
-# 		multiq= Q(Q(customer__email__icontains=q1))
-# 		emailid = ShippingAddress.objects.filter(multiq)
-# 		return render(request, 'store/previous_purchase.html', {'emailid':emailid})
-# 	elif 'q2' in request.GET:
-# 		q2 = request.GET['q2']
-# 		orderid = OrderItem.objects.filter(order__id__icontains=q2)
-# 		return render(request, 'store/previous_purchase.html', {'orderid':orderid})
-# 	else:
-# 		return render(request, 'store/previous_purchase.html')
+
+
+
+@receiver(pre_delete, sender=Product)
+def delete_related_feedbacks(sender, instance, **kwargs):
+    FeedbackTable.objects.filter(product=instance.name).delete()
